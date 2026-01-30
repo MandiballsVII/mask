928120212ebb982ae33d48e851f6cb98df613cd6
ShibaInuEnjoyer <163178983+ShibaInuEnjoyer@users.noreply.github.com>
2026-01-30 22:32:07 +0100

    MainMenu

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
